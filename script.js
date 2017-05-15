3fabe713bc3e5e560fffc3700035e7a003ebb419
Jason Lucas <jlucas5280@gmail.com>
2017-05-15 15:48:35 -0600

    complete phase 1

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ $('.enter-button').on('click', function(){
   console.log('Button Click')
   var websiteTitle = $('.web-title-input').val();
   var websiteURL = $('.web-url-input').val();
-  $('.new-link-container').append(`<article class='link-card'>
-    <h3 class='link-title'>${websiteTitle}</h3>
-    <hr class='hr1'>
-    <a class='the-link' href='www.thewebsiteurl.com'>${websiteURL}</a>
-    <hr class='hr2'>
-    <p class='read'>Read</p>
-    <p class='delete'>Delete</p>
-  </article>`)
+  $('.new-link-container').append(`
+    <article class='link-card'>
+      <h3 class='link-title'>${websiteTitle}</h3>
+      <hr class='hr1'>
+      <a class='the-link' href='www.thewebsiteurl.com'>${websiteURL}</a>
+      <hr class='hr2'>
+      <p class='read'>Read</p>
+      <p class='delete'>Delete</p>
+    </article>`)
+})
+
+$('.new-link-container').on('click', '.delete', function(){
+  console.log ($('.read').parent().children());
+  $(this).parent().remove('.link-card')
 })
 
 $('.new-link-container').on('click', '.read', function(){
   console.log ($('.read').parent().children());
-  $('.link-card').remove('.link-card')
+  $(this).parent().find('.read', '.link-card', '.the-link').toggleClass('read-click');
+  $(this).parent().toggleClass('read-click');
+  $(this).parent().find('.the-link').toggleClass('read-click-underline');
 })
+
+
 
 
 // $(document).ready(function(){
